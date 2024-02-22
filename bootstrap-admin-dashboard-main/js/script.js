f98ab3d6f2a53a0183d008d6862e1eca62539983
22SOECE11080 <135407619+22SOECE11080@users.noreply.github.com>
2024-02-22 12:57:28 +0530

    commit

--- bootstrap-admin-dashboard-main/js/script.js
+++ bootstrap-admin-dashboard-main/js/script.js
@@ -3,38 +3,38 @@ sidebarToggle.addEventListener("click",function(){
     document.querySelector("#sidebar").classList.toggle("collapsed");
 });
 
-document.querySelector(".theme-toggle").addEventListener("click",() => {
-    toggleLocalStorage();
-    toggleRootClass();
-});
+// document.querySelector(".theme-toggle").addEventListener("click",() => {
+//     toggleLocalStorage();
+//     toggleRootClass();
+// });
 
-function toggleRootClass(){
-    const current = document.documentElement.getAttribute('data-bs-theme');
-    const inverted = current == 'dark' ? 'light' : 'dark';
-    document.documentElement.setAttribute('data-bs-theme',inverted);
-}
+// function toggleRootClass(){
+//     const current = document.documentElement.getAttribute('data-bs-theme');
+//     const inverted = current == 'dark' ? 'light' : 'dark';
+//     document.documentElement.setAttribute('data-bs-theme',inverted);
+// }
 
-function toggleLocalStorage(){
-    if(isLight()){
-        localStorage.removeItem("light");
-    }else{
-        localStorage.setItem("light","set");
-    }
-}
+// function toggleLocalStorage(){
+//     if(isLight()){
+//         localStorage.removeItem("light");
+//     }else{
+//         localStorage.setItem("light","set");
+//     }
+// }
 
-function isLight(){
-    return localStorage.getItem("light");
-}
+// function isLight(){
+//     return localStorage.getItem("light");
+// }
 
-if(isLight()){
-    toggleRootClass();
-}
+// if(isLight()){
+//     toggleRootClass();
+// }
 
-function print () {
-    var printDiv = document.getElementById("divPrint");
-    var printWindow = window.open('', '', 'left=0, top=0, width=800, height=500, toolbar=0, scrollbars=0, status=0');
-    printWindow.document.write(printDiv.innerHTML);
-    printWindow.document.close();
-    printWindow.focus();
-    printWindow.print();
-   }
+// function print () {
+//     var printDiv = document.getElementById("divPrint");
+//     var printWindow = window.open('', '', 'left=0, top=0, width=800, height=500, toolbar=0, scrollbars=0, status=0');
+//     printWindow.document.write(printDiv.innerHTML);
+//     printWindow.document.close();
+//     printWindow.focus();
+//     printWindow.print();
+//    }
