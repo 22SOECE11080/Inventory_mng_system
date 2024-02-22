e76280c53783f37f23fb2f37bb82934459706e9c
22SOECE11080 <135407619+22SOECE11080@users.noreply.github.com>
2024-02-22 13:00:25 +0530

    commit

--- bootstrap-admin-dashboard-main/js/script.js
+++ bootstrap-admin-dashboard-main/js/script.js
@@ -14,27 +14,27 @@ function toggleRootClass(){
     document.documentElement.setAttribute('data-bs-theme',inverted);
 }
 
-// function toggleLocalStorage(){
-//     if(isLight()){
-//         localStorage.removeItem("light");
-//     }else{
-//         localStorage.setItem("light","set");
-//     }
-// }
+function toggleLocalStorage(){
+    if(isLight()){
+        localStorage.removeItem("light");
+    }else{
+        localStorage.setItem("light","set");
+    }
+}
 
-// function isLight(){
-//     return localStorage.getItem("light");
-// }
+function isLight(){
+    return localStorage.getItem("light");
+}
 
-// if(isLight()){
-//     toggleRootClass();
-// }
+if(isLight()){
+    toggleRootClass();
+}
 
-// function print () {
-//     var printDiv = document.getElementById("divPrint");
-//     var printWindow = window.open('', '', 'left=0, top=0, width=800, height=500, toolbar=0, scrollbars=0, status=0');
-//     printWindow.document.write(printDiv.innerHTML);
-//     printWindow.document.close();
-//     printWindow.focus();
-//     printWindow.print();
-//    }
+function print () {
+    var printDiv = document.getElementById("divPrint");
+    var printWindow = window.open('', '', 'left=0, top=0, width=800, height=500, toolbar=0, scrollbars=0, status=0');
+    printWindow.document.write(printDiv.innerHTML);
+    printWindow.document.close();
+    printWindow.focus();
+    printWindow.print();
+   }
